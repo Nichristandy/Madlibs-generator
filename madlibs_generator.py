428e74880353a31614d6942528f12b7da79630ef
Nichristandy <65210946+Nichristandy@users.noreply.github.com>
2022-05-13 22:28:30 +0700

    initial commit

---
+++ madlibs_generator.py
@@ -0,0 +1,18 @@
+#madlibs generator
+
+#ask for the user input
+name = input("Input Name: ")
+verb = input("Input verb(past tense): ")
+noun = input("Input noun: ")
+adj = input("Input adjective: ")
+place = input("Input place: ")
+name2 = input("Input Name: ")
+verb2 = input("Input verb(ing): ")
+noun2 = input("Input noun: ")
+
+#print the input
+print("============================================")
+print("Hi My Name is ", name ,"i am ", adj)
+print("This is my", noun, ", oh yeah by the way i live in", place)
+print("i", verb, "yesterday and now i'm", verb2)
+print("And this is my", noun2, "i give it a name and it is ", name2)
